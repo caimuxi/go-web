16816b29c40f33ae3e842c4428437c88d538f45d
root <caimuxi823@gmail.com>
2016-02-18 17:51:53 +0800

    --author=caimuxi <caimuxi@gmail.com>

---
+++ if.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	//var x = 11
+	//if x > 10 {
+	if x := 2;x > 10 {
+		fmt.Println("x is greater than 10")
+	}else{
+		fmt.Println("x is less than 10")
+	}
+}	
